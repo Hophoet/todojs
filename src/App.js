053bdc450cee2d190bb01f79568071dd536c0aaa
hophoet <hophoet@gmail.com>
2020-10-06 18:36:07 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test'
 
 function App() {
   return (
@@ -8,17 +9,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Test/>
       </header>
+      <body>
+       
+      </body>
     </div>
   );
 }
---
+++ src/components/Test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+export default class  App extends React.Component{
+
+    render(){
+
+        return (
+            <div className="App">
+                <p>
+                State <code>if me.wakeup: me.code()</code>.
+                </p>
+                <a
+                className="App-link"
+                href="https://www.instagram.com/hophoet/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                    @hophoet
+                </a>
+            </div>
+        );
+  }
+}
+
